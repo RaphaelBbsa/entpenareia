c73c7bc766cde2ebafc6b51f33f592b55a0eb92b
leonardo melo <leonardomelom@outlook.com>
2024-06-30 21:44:07 -0300

    users in past

--- src/wrongPhones.tsx
+++ src/wrongPhones.tsx
@@ -15,11 +15,28 @@ type clientType = {
 
 export function WrongPhones() {
   const [user, setUser] = useState([])
+  const [pastUsers, setPastUsers] = useState([])
   const [checkInUser, setCheckInUser] = useState([])
   const options = {
     method: 'GET',
     headers: {'Content-Type': 'application/json'}
   };
+  useEffect(() => {
+    fetch(`${api}/past-wrong-phones`, options)
+    .then(response => response.json())
+    .then(response => {
+      setPastUsers(response)
+      console.log(response)
+    })
+    .catch(err => console.error(err));
+  }, [])
+  const sortedPastUsers = [...pastUsers].sort((a:clientType, b:clientType) => {
+    // Convertendo as datas para objetos Date para compará-las
+    const dateA = new Date(a.booking_date);
+    const dateB = new Date(b.booking_date);
+    return dateA.getTime() - dateB.getTime(); // Ordene em ordem crescente, para ordem decrescente, basta trocar para 'dateB.getTime() - dateA.getTime()'
+  });
+
   useEffect(() => {
   fetch(`${api}/wrong-phones`, options)
   .then(response => response.json())
@@ -35,6 +52,7 @@ const sortedUsers = [...user].sort((a:clientType, b:clientType) => {
   const dateB = new Date(b.booking_date);
   return dateA.getTime() - dateB.getTime(); // Ordene em ordem crescente, para ordem decrescente, basta trocar para 'dateB.getTime() - dateA.getTime()'
 });
+
 useEffect(() => {
   fetch(`${api}/check-in-wrong-phones`, options)
   .then(response => response.json())
@@ -51,7 +69,37 @@ const sortedUsersCheckin = [...checkInUser].sort((a:clientType, b:clientType) =>
   return dateA.getTime() - dateB.getTime(); // Ordene em ordem crescente, para ordem decrescente, basta trocar para 'dateB.getTime() - dateA.getTime()'
 });
   return (
-    <main className=' flex flex-col justify-center items-center'>
+    <main className=' flex flex-col justify-center items-center'>   
+      <h2 className=' font-semibold text-2xl my-6'>Check-in's de hoje</h2>
+      {pastUsers.length !== 0 ?
+    <table>
+    <thead>
+      <tr className='border'>
+      <th>Mensagem</th>
+      <th>BotConversa ID</th>
+      <th>Booking Date</th>
+      {/* <th>Check-in Date</th> */}
+      <th>Localizador</th>
+      <th>Email</th>
+      <th>Phone</th>
+      </tr>
+    </thead>
+    <tbody>
+   {sortedPastUsers?.map((client:clientType) => (
+      <tr key={client.id} className='border'>
+        <td className='item'>{client.new_booking_message ? <p>✅</p> : <p>❌</p>}</td>
+        <td className='item'>{client.subscriber_id}</td>
+        <td className='item'>{client.booking_date}</td>
+        {/* <td className='item'>{client.check_in}</td> */}
+        <td className='item'>{client.localizer}</td>
+        <td className='item'>{client.email}</td>
+        <td className='item'>{client.phone}</td>
+      </tr>
+   ))}
+    </tbody>
+  </table> : <h2 className=' text-red-500'>Não há registros</h2>
+    }
+      <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-200" />
       <h2 className=' font-semibold text-2xl my-6'>Reservas de hoje</h2>
     {user.length !== 0 ?
     <table>
